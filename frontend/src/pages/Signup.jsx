832bfdc725f6c9890c1fb39255b8b4a0b2f34baf
Prateek <56671784+prateekguptaims@users.noreply.github.com>
2024-07-22 13:06:39 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -29,7 +29,7 @@ function Signup() {
             return handleError('All fields are required')
         }
         try {
-            const url="http://localhost:8080/auth/signup"
+            const url="https://mern-login-signup-api.vercel.app/auth/signup"
             const response= await fetch(url,{
                 method:'POST',
                 headers:{
